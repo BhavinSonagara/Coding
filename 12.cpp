026f5ae365af21940b549ed0709d971c677792fe
bcsonagara <116618639+bcsonagara@users.noreply.github.com>
2023-09-16 20:49:00 +0530

    Update 12.cpp

--- 12.cpp
+++ 12.cpp
@@ -22,6 +22,7 @@ void solve()
         ans = min(ans, abs(x.first - n));
     }
     cout << ans << endl;
+	//change required 
 }
 int32_t main() {
     ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
